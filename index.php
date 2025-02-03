fdf133c90b24ac18fefc6adc1ba33820b8089043
leuys <34769041+leuys@users.noreply.github.com>
2025-02-03 16:05:33 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+
+Kirby::plugin('studio-bu/onepage', [
+  'hooks' => [
+    'page.render:after' => function ($html, $page) {
+      $scriptTag = '<script>const rootPath="'.kirby()->url().'";const onePageConfig='.json_encode(option('onepage')).';</script>';
+      return str_replace('</head>', $scriptTag.'</head>', $html);
+    }
+  ],
+  'routes' => [
+    [
+      'pattern' => '(:all).json',
+      'action'  => function ($path) {
+        $page = page($path);
+        if( !$page ){
+          return json_encode(['error' => 'Page not found'], 404);
+        }
+
+        $html = $page->render();
+        $dom = new DOMDocument;
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($html);
+        libxml_clear_errors();
+        $xpath = new DOMXPath($dom);
+        $targetElement = $xpath->query('//'.option('onepage.tag'))->item(0);
+        
+        $innerHtml = '';
+        if( option('onepage.inner') ){
+          if ($targetElement) {
+            foreach ($targetElement->childNodes as $child) {
+              $innerHtml .= $dom->saveHTML($child);
+            }
+          }
+        } else {
+          $innerHtml = $dom->saveHTML($targetElement);
+        }
+             
+        $pageModel = $page->toArray();
+        $pageModel['template'] = $page->template()->name();
+        $pageModel['intendedTemplate'] = $page->intendedTemplate()->name();
+        
+        $json[] = [
+          'html' => [
+            'main' => $innerHtml,
+            ],
+          'model' => $pageModel
+        ];
+        return json_encode($json);
+      }
+    ]
+  ]
+]);
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Kirby One Page Plugin
+
+## How to use
+
+1. Add this line to inject the one page javascript. Ideally in the *<head>*
+
+```
+js('/media/plugins/studio-bu/onepage/onepage.js')
+```
+
+2. Add these options to the Kirby *config.php*
+
+```
+'onepage' => [
+    'tag' => 'main', // The HTML element which will be replaced by the new page = DOMXPath querry (i.e. 'div[@id="my-div"]') without '//'
+    'inner' => true, // if the inner html or the whole tag shall be replaced
+    'transitionDuration' => 1000, // The page content exchange transition duration
+    'transitionClass' => 'fade_out' // Body tag class while in transition
+    'activeLinkClass' => 'active_link', // class for internal link once clicked
+],
+```
+
+## License
+
+MIT
+
+## Credits
+
+- [Benjamin Unterluggauer](leuys.com)
